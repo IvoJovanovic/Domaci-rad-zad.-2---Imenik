08478e6493ad2be85d55792cb4e1bc9a4d9db9a6
IvoJovanovic <v-ivo.jovanovic@dump.hr>
2017-09-02 20:25:02 +0200

    Final version

--- scripts/app/studentsList/studentsList.route.js
+++ scripts/app/studentsList/studentsList.route.js
@@ -6,7 +6,8 @@ angular.module('eImenik').config(function ($stateProvider) {
 			template: `
 			<h2>Popis svih učenika: </h2>
 			<hr>			
-            <div ng-repeat="student in students">
+			<div ng-repeat="student in students">
+				<p>{{student.firstName + " " + student.lastName}}</p>
 				<button ui-sref="details({firstName : student.firstName, lastName : student.lastName, finalGrade : student.finalGrade})">O učeniku</button>				
 				<button ng-click="removeStudentFromList(student)">Ukloni učenika</button>
 				<hr>			
